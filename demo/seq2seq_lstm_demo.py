d020044543043c8b079a40f48f29c769b099e203
Ivan Bondarenko <bond005@yandex.ru>
2018-06-17 12:48:58 +0700

    Demo scipt has been imporved (in particular, word correct measure and character correct measure have been added to estimation).

--- demo/seq2seq_lstm_demo.py
+++ demo/seq2seq_lstm_demo.py
@@ -5,7 +5,10 @@ import codecs
 import os
 import pickle
 import sys
+import time
 import random
+
+import numpy as np
 
 try:
     from seq2seq_lstm import Seq2SeqLSTM
@@ -15,6 +18,16 @@ except:
 
 
 def load_text_pairs(file_name):
+    """ Load text pairs from the specified file.
+
+    Each text pair corresponds to a single line in the text file. Both texts (left and right one) in such pair are
+    separated by the tab character. It is assumed that the text file has the UTF-8 encoding.
+
+    :param file_name: name of file containing required text pairs.
+
+    :return a 2-element tuple: the 1st contains list of left texts, the 2nd contains corresponding list of right texts.
+
+    """
     input_texts = list()
     target_texts = list()
     line_idx = 1
@@ -37,6 +50,13 @@ def load_text_pairs(file_name):
 
 
 def shuffle_text_pairs(*args):
+    """ Shuffle elements in lists containing left and right texts for text pairs.
+
+    :param *args: two lists containing left and right texts for text pairs, accordingly.
+
+    :return a 2-element tuple: the 1st contains list of left texts, the 2nd contains corresponding list of right texts.
+
+    """
     assert len(args) == 2, u'Text pairs (input and target texts) are specified incorrectly!'
     indices = list(range(len(args[0])))
     random.shuffle(indices)
@@ -49,6 +69,13 @@ def shuffle_text_pairs(*args):
 
 
 def tokenize_text(src):
+    """ Split up source text by tokens corresponded to single characters with replacing spaces by special token <space>.
+
+    :param src: source text.
+
+    :return token list.
+
+    """
     tokens = list()
     for cur in src.split():
         tokens += list(cur)
@@ -56,6 +83,15 @@ def tokenize_text(src):
     return u' '.join(tokens[:-1])
 
 def detokenize_text(src):
+    """ Join all tokens corresponding to single characters for creating the resulting text.
+
+    This function is reverse for the function `tokenize_text`.
+
+    :param src: source token list.
+
+    :return: the resulting text.
+
+    """
     new_text = u''
     for cur_token in src.split():
         if cur_token == u'<space>':
@@ -65,14 +101,64 @@ def detokenize_text(src):
     return new_text.strip()
 
 def estimate(predicted_texts, true_texts):
-    n_err = 0
-    n_total = len(predicted_texts)
-    for i in range(n_total):
-        pred_ = detokenize_text(predicted_texts[i]).lower()
-        true_ = detokenize_text(true_texts[i]).lower()
-        if pred_ != true_:
-            n_err += 1
-    return 1.0 - (n_err / float(n_total))
+    """
+
+    :param predicted_texts: list of all predicted texts.
+    :param true_texts: list of all true texts, corresponding to predicted texts.
+
+    :return: a 3-element tuple, which includes three measures: sentence correct, word correct and character correct.
+
+    """
+    n_corr_sent = 0
+    n_corr_word = 0
+    n_corr_char = 0
+    n_total_sent = len(predicted_texts)
+    n_total_word = 0
+    n_total_char = 0
+    for i in range(n_total_sent):
+        pred_ = detokenize_text(predicted_texts[i])
+        true_ = detokenize_text(true_texts[i])
+        if pred_ == true_:
+            n_corr_sent += 1
+            n_corr_word += len(true_.split())
+            n_corr_char += len(true_)
+        else:
+            n_corr_word += (len(true_.split()) - calc_levenshtein_dist(true_.split(), pred_.split()))
+            n_corr_char += (len(true_) - calc_levenshtein_dist(list(true_), list(pred_)))
+        n_total_word += len(true_.split())
+        n_total_char += len(true_)
+    return n_corr_sent / float(n_total_sent), n_corr_word / float(n_total_word), n_corr_char / float(n_total_char)
+
+
+def calc_levenshtein_dist(left_list, right_list):
+    """ Calculate the Levenshtein distance between two lists.
+
+    See https://martin-thoma.com/word-error-rate-calculation/
+
+    :param left_list: left list of tokens.
+    :param right_list: right list of tokens.
+
+    :return total number of substitutions, deletions and insertions required to change one list into the other.
+
+    """
+    d = np.zeros((len(left_list) + 1) * (len(right_list) + 1), dtype=np.uint32)
+    d = d.reshape((len(left_list) + 1, len(right_list) + 1))
+    for i in range(len(left_list) + 1):
+        for j in range(len(right_list) + 1):
+            if i == 0:
+                d[0][j] = j
+            elif j == 0:
+                d[i][0] = i
+    for i in range(1, len(left_list)+1):
+        for j in range(1, len(right_list)+1):
+            if left_list[i-1] == right_list[j-1]:
+                d[i][j] = d[i-1][j-1]
+            else:
+                substitution = d[i-1][j-1] + 1
+                insertion    = d[i][j-1] + 1
+                deletion     = d[i-1][j] + 1
+                d[i][j] = min(substitution, insertion, deletion)
+    return d[len(left_list)][len(right_list)]
 
 
 def main():
@@ -132,8 +218,10 @@ def main():
                 pickle.dump(seq2seq, fp)
             print(u'Model has been successfully saved into file "{0}".'.format(model_name))
 
+    start_time = time.time()
     predicted_texts = seq2seq.predict(input_texts_for_testing)
-    sentence_correct = estimate(predicted_texts, target_texts_for_testing)
+    end_time = time.time()
+    sentence_correct, word_correct, character_correct = estimate(predicted_texts, target_texts_for_testing)
     print(u'')
     print(u'{0} texts have been predicted.'.format(len(predicted_texts)))
     print(u'Some samples of predicted text pairs:')
@@ -143,6 +231,10 @@ def main():
         print(u'    ' + detokenize_text(input_text) + u'\t' + detokenize_text(target_text))
     print(u'')
     print(u'Total sentence correct is {0:.2%}.'.format(sentence_correct))
+    print(u'Total word correct is {0:.2%}.'.format(word_correct))
+    print(u'Total character correct is {0:.2%}.'.format(character_correct))
+    print(u'')
+    print(u'Mean time of sentence prediction is {0:.3} sec.'.format((end_time - start_time) / len(predicted_texts)))
 
 
 if __name__ == '__main__':
